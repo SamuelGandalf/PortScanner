c4263a504a0a55e875b3a7a3315fc895122468d6
Samuel Gandaanuo <84130650+SamuelGandalf@users.noreply.github.com>
2022-10-15 21:48:49 -0400

    Update Port_Scanner1.py

--- Port_Scanner1.py
+++ Port_Scanner1.py
@@ -7,12 +7,12 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #this is to create the 
 socket.setdefaulttimeout(15) #this is to perform the scan within the specifid time.
 
 host = input("[**] Enter The Host To Perform Scan: ")
-#port = int(raw_input("[**] Enter the Port To Scan: "))
+#port = int(nput("[**] Enter the Port To Scan: "))
 
 def Port_Scanner(port):
 	if sock.connect_ex((host, port)):
 		print(colored("Port %d is closed!" %(port), 'red'))
 	else:
 		print(colored("Port %d is opened!" %(port), 'green'))
-for port in range(1, 100):
+for port in range(1, 100): #range of the ports to be scanned
 	Port_Scanner(port)

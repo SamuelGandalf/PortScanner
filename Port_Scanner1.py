358966fcbad2be4ab173d75ec456abe287d02472
SamuelGandalf <samuelgandalf4656@gmail.com>
2022-10-15 21:12:26 -0400

    allows user to input the the host to be scanned, and also allow for scannig a wide range of ports

--- Port_Scanner1.py
+++ Port_Scanner1.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python
 
 import socket
+from termcolor import colored
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = "172.18.112.1"
-port = 135
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #this is to create the socket object
+socket.setdefaulttimeout(15) #this is to perform the scan within the specifid time.
+
+host = input("[**] Enter The Host To Perform Scan: ")
+#port = int(raw_input("[**] Enter the Port To Scan: "))
 
 def Port_Scanner(port):
 	if sock.connect_ex((host, port)):
-		print "Port %d is closed!" %(port)
+		print(colored("Port %d is closed!" %(port), 'red'))
 	else:
-		print "Port %d is opened!" %(port)
-Port_Scanner(port)
+		print(colored("Port %d is opened!" %(port), 'green'))
+for port in range(1, 100):
+	Port_Scanner(port)
